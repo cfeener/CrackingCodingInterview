8c191a477576154d77dd560018c8a7579b2165c0
Christopher Feener <christopher.feener@outlook.com>
2019-05-06 11:54:22 -0700

    Will implement escape characters.

--- chap1/p1_3_url.c
+++ chap1/p1_3_url.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main (void) {
 	char * url = "Mr John Smith   ";
 
 	int n = strlen(url);
 
+	char * delim = " ";
+	char * token = strtok(url, delim);
+	printf("%s", token);
+	token = strtok(NULL, delim);
+
+	while (token != NULL) {
+		printf("\%20%s", token);
+		token = strtok(NULL, delim);
+	}
+
+	return 0;
 }
