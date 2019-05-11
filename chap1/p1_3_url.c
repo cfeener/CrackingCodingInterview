cb272d5e0e397bd4945597b540973f330303cb2a
Christopher Feener <christopher.feener@outlook.com>
2019-05-10 21:03:05 -0700

    Complete #3. Streamline trailing-space error check.

--- chap1/p1_3_url.c
+++ chap1/p1_3_url.c
@@ -3,23 +3,47 @@
 #include <string.h>	//For strlen()
 #include <assert.h>	//For assert()
 
-char * urlify(char * str) {
+#define ADDED_CHARS 2
+
+int spacesExpected(char * str) {
 	int n = strlen(str);
-	char * url = calloc(n, sizeof(char));
+	int true_spaces = 0;
 
 	int i = n - 1;
 	while (str[i] == ' ')
-		i--;	//Iterate to last character
+		i--;
 
+	while (i >= 0) {
+		if (str[i] == ' ')
+			true_spaces++;
+		i--;
+	}
+
+	return ADDED_CHARS * true_spaces;
+}
+
+
+char * urlify(char * str) {
+	int n = strlen(str);
+	int i = n - 1, spaces = 0;
+
+	while (str[i] == ' ') {
+		spaces++;
+		i--;	//Iterate to last character
+	}
+	if (spaces != spacesExpected(str)) {	//Main error check
+		printf("Wrong number of trailing spaces in initial sentence.\n");
+		exit(1);
+	}
+
+	char * url = calloc(n, sizeof(char));
 	int j = n - 1;
 	while (i >= 0) {
 		if (str[i] == ' ') {
-			assert(j > 2);
 			url[j - 2] = '%'; url[j - 1] = '2'; url[j] = '0';
 			j -= 3;
-		} else {
+		} else 
 			url[j--] = str[i];
-		}
 		i--;
 	}
 
@@ -27,7 +51,7 @@ char * urlify(char * str) {
 }
 
 int main (void) {
-	char * str = "Mr John Smith    ";
+	char * str = "Mr John Smith    ";	//Since this has 2 real spaces, there are 4 spaces at the end.
 
 	char * url = urlify(str);
 	printf("%s\n", url);
