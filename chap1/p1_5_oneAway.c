26f57e5e311f276445226cecb50e9fdd10db91c9
Christopher Feener <christopher.feener@outlook.com>
2019-05-12 23:24:37 -0700

    Program counts matching frequencies, but not letters. Will fix.

--- chap1/p1_5_oneAway.c
+++ chap1/p1_5_oneAway.c
@@ -3,8 +3,8 @@
  * Verfies that two words have at most one different character.
  */
 
-#define STRING1 "ple"
-#define STRING2 "pale"
+#define STRING1 "This will be awesome!"
+#define STRING2 "This will be awesome!"
 
 #include <stdio.h>
 #include <string.h>	//For strlen()
@@ -15,10 +15,10 @@
 
 #define ALPHA 26 	//Size of alphabet.
 
-bool diff (char * s1, char * s2) {
+bool oneAway (char * s1, char * s2) {
 	assert(s1 != NULL && s2 != NULL);
 
-	int n1 = strlen(s1), n2 = strlen(s2);
+	int n1 = strlen(s1), n2 = strlen(s2), matches = 0;
 	int freq1[ALPHA], freq2[ALPHA];
 	int i;
 	for (i = 0; i < ALPHA; i++)
@@ -30,16 +30,13 @@ bool diff (char * s1, char * s2) {
 	for (i = 0; i < n2; i++)
 		freq2[tolower(s2[i]) - 'a']++;
 
-	bool isUnequal = false;
-	for (i = 0; i < ALPHA; i++) {	//Traverse frequencies to make sure there is only a one letter difference (or less.)
-		if (freq1[i] != freq2[i]) {	//If unequal
-			if (isUnequal)		//If another inequality already exists, false
-				return false;
-			if (abs(freq1[i] - freq2[i]) > 1)	//Otherwise, inequality must be 1
-				return false;
-			isUnequal = true;
-		}
+	for (i = 0; i < ALPHA; i++) {	//Count matches
+		if ((freq1[i] != 0) && (freq1[i] == freq2[i]))	//Must be a non-zero match
+			matches++;
 	}
+
+	if ((abs(n1 - matches) > 1) || (abs(n2 - matches) > 1))
+		return false;	//If more than one letter was not a match, false
 
 	return true;
 }
@@ -48,7 +45,7 @@ int main (void) {
 	char * s1 = STRING1;
 	char * s2 = STRING2;
 
-	if (diff(s1, s2))
+	if (oneAway(s1, s2))
 		printf("True!\n");
 	else 
 		printf("False.\n");
