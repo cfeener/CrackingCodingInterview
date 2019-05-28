be4cf958666b96749b96db95b506db36aa59dd85
Christopher Feener <christopher.feener@outlook.com>
2019-05-28 11:53:12 -0700

    Start #7: Rotate matrix 90-degrees. Made 2 notes on algorithm.

---
+++ chap1/p1_7_rotateMatrix.c
@@ -0,0 +1,13 @@
+/* Christopher Feener
+ * Started on 5/28/2019
+ * Rotates an array of 4-byte values 90-degrees.
+ */
+
+#include <stdio.h>
+
+//TODO: Swap 4 variables in much the same way as swapping 2 variables (5 lines), 
+//	OR 'xor'-swap 3 times (3 * 3 lines).
+
+int main (void) {
+	return 0;
+}
