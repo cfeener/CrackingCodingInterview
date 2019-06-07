b0491944abece76b058c579d052b544fdd72ded8
Christopher Feener <christopher.feener@outlook.com>
2019-06-06 21:47:07 -0700

    Implement zero'ing specific rows and columns, and test on larger matrix.

--- chap1/p1_8_zeroMatrix.c
+++ chap1/p1_8_zeroMatrix.c
@@ -2,31 +2,39 @@
  * Started on June 4, 2019
  * Zero entire row and column of a matrix where an entry is zero.
  */
-#define N 3
-#define M 3
+#define N 10
+#define M 10
 
 #include <stdio.h>
 #include <stdlib.h> 	//For calloc() and free()
+#include <stdbool.h>	//For true, false
+#include <time.h>	//For time()
 
 void printMatrix(unsigned int * matrix, unsigned int n, unsigned int m) {
         int i, j;
         for (i = 0; i < n; i++) {
                 for (j = 0; j < m; j++)
-                        printf("%i ", matrix[i * n + j]);
+                        printf("%02i ", matrix[i * n + j]);
                 printf("\n");
         }
 }
 
-void process(unsigned int * matrix, unsigned int n, unsigned int m) {
+void process(unsigned int * matrix, bool * is_done, unsigned int n, unsigned int m) {
 	int i, j;
 	int a, b;
 	for (i = 0; i < n; i++) {
 		for (j = 0; j < m; j++) {
-			if (matrix[n * i + j] == 0) {
-				for (a = 0; a < n; a++)
+			if (is_done[n * i + j])
+				continue;
+			else if (matrix[n * i + j] == 0) {
+				for (a = 0; a < n; a++) {
 					matrix[n * a + j] = 0;
-				for (b = 0; b < m; b++)
+					is_done[n * a + j] = true;
+				}
+				for (b = 0; b < m; b++) {
 					matrix[n * i + b] = 0;
+					is_done[n * i + b] = true;
+				}
 			}
 		}
 	}
@@ -35,19 +43,25 @@ void process(unsigned int * matrix, unsigned int n, unsigned int m) {
 int main (void) {
 	unsigned int n = N, m = M;
 	unsigned int * matrix = (unsigned int *)calloc(n * m, sizeof(unsigned int));
+	bool * is_done = (bool *)calloc(n * m, sizeof(bool));
 
 	//Initialize:
+	srand(time(NULL));
 	unsigned int i, j;
 	for (i = 0; i < n; i++) {
 		for (j = 0; j < m; j++) {
-			matrix[n * i + j] = n * i + j;
+			if ((i == 3 && j == 7) || (i == 8 && j == 1))
+				matrix[n * i + j] = 0;
+			else
+				matrix[n * i + j] = rand() % 90 + 10;
+			is_done[n * i + j] = false;
 		}
 	}
 
 	printf("Before: \n");
 	printMatrix(matrix, n, m);
 
-	process(matrix, n, m);
+	process(matrix, is_done, n, m);
 
 	printf("After: \n");
 	printMatrix(matrix, n, m);
