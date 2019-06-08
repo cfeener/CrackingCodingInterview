af866f766079732937b9525c76f2b294a7a5ab85
Christopher Feener <christopher.feener@outlook.com>
2019-06-08 16:52:29 -0700

    Testing Eclipse and Java
    Created classes and getters and setters.

---
+++ chap2/p2_1_removeDups/src/p2_1_removeDups/Main.java
@@ -0,0 +1,11 @@
+package p2_1_removeDups;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing in main...");
+
+	}
+
+}
---
+++ chap2/p2_1_removeDups/src/p2_1_removeDups/removeDups.java
@@ -0,0 +1,15 @@
+package p2_1_removeDups;
+
+import java.util.LinkedList;
+
+public class removeDups {
+	LinkedList <Integer> ll = new LinkedList<Integer>();
+
+	public LinkedList<Integer> getLl() {
+		return ll;
+	}
+
+	public void setLl(LinkedList<Integer> ll) {
+		this.ll = ll;
+	}
+}
