5e0e8585525df779cc0093494d6d9ececba801cf
Christopher Feener <christopher.feener@outlook.com>
2019-06-11 11:43:35 -0700

    Set up Eclipse, experiment with removeDups class.

--- chap2/p2_1_removeDups/src/p2_1_removeDups/removeDups.java
+++ chap2/p2_1_removeDups/src/p2_1_removeDups/removeDups.java
@@ -12,4 +12,13 @@ public class removeDups {
 	public void setLl(LinkedList<Integer> ll) {
 		this.ll = ll;
 	}
+	
+	void add(Integer i) {
+		this.add(i);
+	}
+	
+	void set(int x) {
+		assert(this.ll.get(x) != null);
+		this.ll.set(x, -1);	//Mark as duplicate; will not copy into new array.
+	}
 }
